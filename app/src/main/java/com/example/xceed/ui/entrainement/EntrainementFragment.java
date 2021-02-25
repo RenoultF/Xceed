07000447a131153ed88451ed1c94814cf5b5041a
RenoultF <florent.renoult.etu@univ-lemans.fr>
2021-02-25 10:34:46 +0100

    MAJ frag entrainement

--- app/src/main/java/com/example/xceed/ui/entrainement/EntrainementFragment.java
+++ app/src/main/java/com/example/xceed/ui/entrainement/EntrainementFragment.java
@@ -1,30 +1,31 @@
-package com.example.xceed.ui.exercice;
+package com.example.xceed.ui.entrainement;
 
 import android.os.Bundle;
-        import android.view.LayoutInflater;
-        import android.view.View;
-        import android.view.ViewGroup;
-        import android.widget.TextView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
 
-        import androidx.annotation.NonNull;
-        import androidx.annotation.Nullable;
-        import androidx.fragment.app.Fragment;
-        import androidx.lifecycle.Observer;
-        import androidx.lifecycle.ViewModelProvider;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
 
-        import com.example.xceed.R;
+import com.example.xceed.R;
+import com.example.xceed.ui.exercice.ExerciceViewModel;
 
 public class EntrainementFragment extends Fragment {
 
-    private ExerciceViewModel exerciceViewModel;
+    private ExerciceViewModel entrainementViewModel;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        exerciceViewModel =
+        entrainementViewModel =
                 new ViewModelProvider(this).get(ExerciceViewModel.class);
         View root = inflater.inflate(R.layout.fragment_entrainement, container, false);
         final TextView textView = root.findViewById(R.id.text_dashboard);
-        exerciceViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
+        entrainementViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
             @Override
             public void onChanged(@Nullable String s) {
                 textView.setText(s);
--- app/src/main/java/com/example/xceed/ui/entrainement/EntrainementViewModel.java
+++ app/src/main/java/com/example/xceed/ui/entrainement/EntrainementViewModel.java
@@ -1,4 +1,4 @@
-package com.example.xceed.ui.exercice;
+package com.example.xceed.ui.entrainement;
 
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
