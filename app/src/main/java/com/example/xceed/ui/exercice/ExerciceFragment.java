449d98855deaaa61fc6ee9f9d6fc94bbe7e3194b
RenoultF <florent.renoult.etu@univ-lemans.fr>
2021-03-15 16:41:43 +0100

    fin info exercice

--- app/src/main/java/com/example/xceed/ui/exercice/ExerciceFragment.java
+++ app/src/main/java/com/example/xceed/ui/exercice/ExerciceFragment.java
@@ -75,6 +75,35 @@ public class ExerciceFragment extends Fragment {
         listExercice.add(new Exercice("Pompe","Ceci est des pompe",R.drawable.gif_pompes, "Pectoraux"));
         listExercice.add(new Exercice("Ecarté (Haltere)","Ceci est de l'écarté",R.drawable.gif_ecarte, "Pectoraux"));
         listExercice.add(new Exercice("Ecarté (Poulie)","Ceci est de l'écarté poulie",R.drawable.gif_ecarte_poulie, "Pectoraux"));
+        listExercice.add(new Exercice("Developpé couché Haltere","Ceci est de du dev couch haltere",R.drawable.gif_dc, "Pectoraux"));
+        listExercice.add(new Exercice("Developpé incliné Haltere","Ceci est de du dev couch haltere",R.drawable.gif_developpe_incline_haltere, "Pectoraux (Haut)"));
+        listExercice.add(new Exercice("Butterfly","Ceci est de du ButterFly",R.drawable.gif_butterfly, "Pectoraux"));
+        listExercice.add(new Exercice("Traction","Ceci est de des Tractions",R.drawable.gif_traction, "Dos (Grand Dorsal)"));
+        listExercice.add(new Exercice("Tirage Vertical","Ceci est du tirage Vertical",R.drawable.gif_tirage_vertical, "Dos (Grand Dorsal)"));
+        listExercice.add(new Exercice("Rowing (Barre)","Ceci est du Rowing Barre",R.drawable.gif_rowing, "Dos (Grand Dorsal)"));
+        listExercice.add(new Exercice("Tirage Bucheron (Haltere)","Ceci est du Tirage Bucheron",R.drawable.gif_tirage_bucheron, "Dos (Grand Dorsal)"));
+        listExercice.add(new Exercice("Tirage Verticale Prise Serrée (Poulie)","Ceci est Tirage Verticale Prise Serrée",R.drawable.gif_tirage_vertical, "Dos (Grand/Petit Rond)"));
+        listExercice.add(new Exercice("Soulever de Terre (Barre)","Ceci est Soulever de Terre",R.drawable.gif_soulever_de_terre, "Dos (Lombaire)"));
+        listExercice.add(new Exercice("Curl (Barre)","Ceci est du curl",R.drawable.gif_curl_barre, "Biceps"));
+        listExercice.add(new Exercice("Curl Concentré (Haltere)","Ceci est du curl concentrée",R.drawable.gif_curl_concentre, "Biceps"));
+        listExercice.add(new Exercice("Curl Prise Marteau (Haltere)","Ceci est du curl concentrée marteau",R.drawable.gif_prise_marteau, "Biceps"));
+        listExercice.add(new Exercice("Curl Larry Scott (Haltere)","Ceci est du curl Larry Scott",R.drawable.temp, "Biceps"));
+        listExercice.add(new Exercice("Dips","Ceci est du dips",R.drawable.gif_dips, "Triceps/Pectoraux"));
+        listExercice.add(new Exercice("Extension couché (Barre)","Ceci est du extension couché",R.drawable.gif_extension_couche, "Triceps"));
+        listExercice.add(new Exercice("Extension (Poulie)","Ceci est du extension poulie",R.drawable.gif_extension_poulie, "Triceps"));
+        listExercice.add(new Exercice("Extension inclinée (Poulie)","Ceci est du extension inclinée poulie",R.drawable.gif_extension_incline, "Triceps"));
+        listExercice.add(new Exercice("Extension Verticale (Haltere)","Ceci est du extension Verticale Haltere",R.drawable.gif_extension_verticale, "Triceps"));
+        listExercice.add(new Exercice("Squat (Barre)","Ceci est du Squat",R.drawable.gif_squat, "Fessier/Jambe"));
+        listExercice.add(new Exercice("Soulever de Terre Jambes Tendues (Barre)","Ceci est du Soulever de Terre Jambes Tendues",R.drawable.gif_soulever_de_terre_jambe_tendues, "Fessier/Jambe"));
+        listExercice.add(new Exercice("Leg Extension","Ceci est du Leg Extension",R.drawable.gif_leg_extension, "Jambe"));
+        listExercice.add(new Exercice("Press Cuisse","Ceci est du Press Cuisse",R.drawable.gif_press_cuisse, "Jambe"));
+        listExercice.add(new Exercice("Leg Curl","Ceci est du Leg Curl",R.drawable.gif_leg_curl, "Jambe"));
+        listExercice.add(new Exercice("Fente (Haltere)","Ceci est des fentes",R.drawable.gif_fente, "Jambe"));
+
+
+
+
+
 
 
 
