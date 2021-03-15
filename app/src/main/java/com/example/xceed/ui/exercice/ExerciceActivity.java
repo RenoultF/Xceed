4791b94fcd2cd6d354a902074782086bf4109886
RenoultF <florent.renoult.etu@univ-lemans.fr>
2021-03-15 16:10:06 +0100

    pb resolu

--- app/src/main/java/com/example/xceed/ui/exercice/ExerciceActivity.java
+++ app/src/main/java/com/example/xceed/ui/exercice/ExerciceActivity.java
@@ -1,10 +1,9 @@
 package com.example.xceed.ui.exercice;
 import android.app.Activity;
 import android.content.Intent;
-<<<<<<< HEAD
-=======
+
 import android.graphics.drawable.Drawable;
->>>>>>> cbb9a97cf884213b30fdf68b7de192a8af412d26
+
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -19,9 +18,6 @@ import org.w3c.dom.Text;
 import java.util.ArrayList;
 
 public class ExerciceActivity extends Activity {
-<<<<<<< HEAD
-
-    public static int draws[]={R.drawable.gif_dc,R.drawable.gif_devinc,R.drawable.gif_devdec,R.drawable.gif_pompes,R.drawable.gif_ecarte};
     Intent intent;
     int buttonId;
     int indiceExercice;
@@ -31,25 +27,16 @@ public class ExerciceActivity extends Activity {
     private TextView text_description;
     private TextView text_categorie;
 
-=======
-    public static int draws[]={R.drawable.gif_dc,R.drawable.gif_devinc,R.drawable.gif_devdec,R.drawable.gif_pompes,R.drawable.gif_ecarte};
-    Intent intent;
-    int buttonId;
->>>>>>> cbb9a97cf884213b30fdf68b7de192a8af412d26
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         intent = getIntent();
-
-<<<<<<< HEAD
-        intent = getIntent();
-=======
->>>>>>> cbb9a97cf884213b30fdf68b7de192a8af412d26
         buttonId = intent.getIntExtra("ButtonId", 0);
 
         setContentView(R.layout.exercize_info);
 
-<<<<<<< HEAD
+
         gifImageView = (GifImageView) findViewById(R.id.GifImageView);
         text_exercice = (TextView) findViewById(R.id.txt_exercice);
         text_description = (TextView) findViewById(R.id.txt_descriptif);
@@ -152,27 +139,5 @@ public class ExerciceActivity extends Activity {
         text_exercice.setText(exerciceCourant.getNomExerciceEx());
         text_categorie.setText(exerciceCourant.getCategorie());
         text_description.setText(exerciceCourant.getDescriptionExercice());
-=======
-            GifImageView gifImageView = (GifImageView) findViewById(R.id.GifImageView);
-            switch (buttonId){
-                case R.id.button:
-                    gifImageView.setGifImageResource(draws[0]);
-                    break;
-                case R.id.button2:
-                    gifImageView.setGifImageResource(draws[1]);
-                    break;
-                case R.id.button3:
-                    gifImageView.setGifImageResource(draws[2]);
-                    break;
-                case R.id.button4:
-                    gifImageView.setGifImageResource(draws[3]);
-                    break;
-                case R.id.button5:
-                    gifImageView.setGifImageResource(draws[4]);
-                    break;
-
-
-        }
->>>>>>> cbb9a97cf884213b30fdf68b7de192a8af412d26
     }
 }
